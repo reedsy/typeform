b00ff5d152ce20a74ec01cc59f68a3dc71fdcf34
Matt Cobb <contact@matthewcobb.co.uk>
2016-07-19 12:52:51 +0100

    Add grunt connect server

--- gruntfile.js
+++ gruntfile.js
@@ -11,17 +11,25 @@ module.exports = function (grunt) {
     // SASS task config
     sass: {
       dev: {
-        options: {
-          sourcemap: "auto"
-        },
         files: {
             // destination         // source file
             "stylesheets/main.css" : "scss/main.scss"
         }
       }
     },
+    connect: {
+      server: {
+        options: {
+          keepalive: true,
+          open: true
+        }
+      }
+    }
   });
+
+  grunt.registerTask('default', ['watch', 'connect']);
 
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
 };
